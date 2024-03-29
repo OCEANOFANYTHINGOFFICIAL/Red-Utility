6d2692a2b0bd5c289ae86e4cff5cb0cb60e5c1b3
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2024-03-29 22:43:46 +0530

    Update lazyLoading.js

--- js/lazyLoading.js
+++ js/lazyLoading.js
@@ -1,13 +1,1 @@
-window.addEventListener("load", function() {
-    var lazyImages = document.querySelectorAll('.lazy');
-    function lazyLoad() {
-        lazyImages.forEach(function(img) {
-        var rect = img.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight && getComputedStyle(img).display !== 'none') {
-            img.src = img.getAttribute('alt');
-            img.classList.remove('lazy');
-        }
-    });
-    }
-    window.addEventListener('scroll', lazyLoad);
-});
+// nothing to show
